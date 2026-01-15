e43101835de3270b42eaae6123296a6f7872881c
phaANTom2303 <anikavy.goenka@gmail.com>
2026-01-15 20:08:12 +0530

    server middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;  
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.get("/", (req,res) =>{
     res.send("Hello.world");
 })
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
